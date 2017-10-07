b9bd4b42bd21301f9231233259b752d6d0776acb
Trent Cooper <coop741@gmail.com>
2017-10-07 10:51:32 -0700

    Added package.json, basic functionality is working

--- FlashCardGenerator.js
+++ FlashCardGenerator.js
@@ -2,11 +2,11 @@ var inquirer = require("inquirer");
 var BasicCard = require("./BasicCard");
 var ClozeCard = require("./ClozeCard");
 
- 
+//Start flash card generator
 function start() {
   inquirer.prompt({
       name: "card",
-      type: "rawlist",
+      type: "input",
       message: "Would you like to create a BASIC card or a CLOZE card?",
       choices: ["BASIC", "CLOZE"]
     })
@@ -38,12 +38,51 @@ function createBasic() {
     },
   ]).then(function(answers) {
     var newCard = new BasicCard(answers.question, answers.answer);
-    console.log(newCard.front);
-    console.log(newCard.back);
-    another();
+    console.log("Question: " + newCard.front);
+    console.log("Answer: " + newCard.back);
+    createANewFlashCard();
   });
 }
 
+//Create a cloze card
+function createCloze() {
+  inquirer.prompt([{
+      name: "questionStatement",
+      type: "input",
+      message: "Please input a question statement."
+    },
+    {
+      name: "cloze",
+      type: "input",
+      message: "Which part of this statement is the answer?"
+    },
+  ]).then(function(answers) {
+    var newCard = new ClozeCard(answers.questionStatement, answers.cloze);
+      if (answers.questionStatement === answers.cloze) {
+        console.log("Try again, partial answer can't be the full question statement"); 
+    } else {
+        console.log("Question Statement: " + newCard.partial);
+        console.log("Answer: " + newCard.cloze);
+    }
+    createANewFlashCard();
+  });
+}
+
+
+function createANewFlashCard() {
+  inquirer.prompt({
+      name: "newCard",
+      type: "input",
+      message: "Would you like to create a new flash card?"
+    })
+    .then(function(answer) {
+      if (answer.newCard.toLowerCase() === "yes" || answer.newCard.toLowerCase() === "y") {
+        start();
+      } else {
+        console.log("Thank you for creating flash cards with me.")
+      }
+    });
+}
 
 
 
